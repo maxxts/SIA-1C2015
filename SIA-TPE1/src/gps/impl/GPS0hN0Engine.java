426d0cee1fa43dd3b705f458377abffe623a131a
maxxts <maxxts@gmail.com>
2015-03-29 21:51:37 -0300

    Iterative strategy implemented (Not tested)

--- SIA-TPE1/src/gps/impl/GPS0hN0Engine.java
+++ SIA-TPE1/src/gps/impl/GPS0hN0Engine.java
@@ -11,6 +11,8 @@ public class GPS0hN0Engine extends GPSEngine {
 		
 		System.out.println("Executing " + this.getStrategy().toString() + " addNode step...");
 		
+		// TODO: Ver de sacar el caso base
+		
 		if (this.getStrategy().equals(SearchStrategy.BFS))
 		{
 			this.getOpen().add(node);
@@ -20,20 +22,78 @@ public class GPS0hN0Engine extends GPSEngine {
 		{
 			
 			if(this.getOpen().size() == 0){
+				
 				this.getOpen().add(node);
+			
+			}else{
+				
+				int i = 0;
+				while (this.getOpen().get(i).getParent().equals(node.getParent()))
+				{
+					i++;
+				}
+				
+				this.getOpen().add(i,node);
 			}
 			
-			int i = 0;
-			while (this.getOpen().get(i).getParent().equals(node.getParent()))
-			{
-				i++;
-			}
 			
-			this.getOpen().add(i,node);
 		}
 		
 		if(this.getStrategy().equals(SearchStrategy.Iterative))
 		{
+			if (node.getParent().equals(null) && this.getOpen().size() == 0)
+			{
+				this.getOpen().add(node);
+				return;
+			}
+			
+			// Search for parent, self or parent's brother in open list.
+			
+			boolean parentIsOpen = false;
+			boolean nodeIsOpen = false;
+			boolean parentBrotherIsOpen = false;
+			int i = 0;
+			int lastParentBrotherIndex = 0;
+			
+			for (GPSNode aNode : this.getOpen())
+			{
+				if (node.getParent().equals(aNode))
+				{
+					parentIsOpen = true;
+				}
+				if(node.equals(aNode))
+				{
+					nodeIsOpen = true;
+				}
+				if(node.getParent().getParent().equals(aNode.getParent()))
+				{
+					parentBrotherIsOpen = true;
+					lastParentBrotherIndex = i;
+					
+				}
+				i++;
+			}
+			
+			// If my parent is not open, I have to re-add it to the list for next level.
+			if (!parentIsOpen)
+			{
+				// If my parent's brother is open, I add my parent after it
+				if (parentBrotherIsOpen)
+				{
+					this.getOpen().add(lastParentBrotherIndex + 1, node.getParent());
+				}
+				else
+				{
+					this.getOpen().add(node.getParent());
+				}
+				
+			}
+			
+			// If self node is not open, I add at the end of the list
+			if (!nodeIsOpen) 
+			{
+				this.getOpen().add(node);
+			}			
 			
 		}
 				
